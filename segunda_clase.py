66bbc4ba53c525caab2ae6095c703a8c454fc2b9
lisonmon <allison.ubeda@ug.uchile.cl>
2025-04-11 17:23:55 +0000

    cambios en clase

---
+++ segunda_clase.py
@@ -0,0 +1,26 @@
+print("suma")
+print("suma", 7+1)
+print("vamos a ver division por piso",7//2) #lo convierte en numero entero
+print("operacion de porcentaje",7%4)#indica el residuo de la division
+print("esponencial",3**2)
+
+
+#########String o cadena de texto
+print("vamos a ver como trabajar con texto y como lee las cadenas de texto en codigos")
+texto = "esto es ota linea, \n holaa"
+print("texto")
+print(" wste texto\texto")
+texto_2="eeste texto sirve para trabajar en string"
+print(texto_2, numero)
+numero= 80
+
+###funcion f:
+
+print(f"eeste texto sirve para trabajar en string")
+
+
+
+
+
+#############a##################
+
